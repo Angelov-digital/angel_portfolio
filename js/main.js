c6aa15da63e2c1b499be95a75ab366d839f846d5
Angelov-digital <96943583+Angelov-digital@users.noreply.github.com>
2023-03-21 19:01:35 +0100

    REV3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ myID = document.getElementById("customID");
 
 var myScrollFunc = function() {
   var y = window.scrollY;
-  if (y >= 930) {
+  if (y >= 860) {
     myID.className = "cta show"
   } else {
     myID.className = "cta hide"
